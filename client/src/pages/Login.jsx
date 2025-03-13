41516db56f4290af27020d0a328079705ba96313
Nikhil <nikhlrajkumarjee3@gmail.com>
2025-03-13 14:49:37 +0530

    navbar components

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -33,7 +33,7 @@ const Login = () => {
               Manage all your task in one place!
             </span>
             <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700'>
-              <span>Cloud-Based</span>
+              <span>Zidio</span>
               <span>Task Manager</span>
             </p>
 
